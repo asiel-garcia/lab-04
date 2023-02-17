9558849b92a4169541d5a8f2882bf1e68772f9bf
asielgar <asielgar@usc.edu>
2023-02-17 05:53:59 -0800

    rpi broker code

--- vm_cont_chain.py
+++ vm_cont_chain.py
@@ -1,24 +1,28 @@
 import paho.mqtt.client as mqtt
+import time
 
-def on_connect(clients, userdata, flags, rc):
+def on_connect(client, userdata, flags, rc):
 	print("connected to server with result code"+str(rc))
 	
 	client.subscribe("asielgar/ping")
 	
-	client.message_callback_add("user/ping",on_message_from_ping)
+	client.message_callback_add("asielgar/ping",on_message_from_ping)
 
 def on_message(client, userdata, msg):
 	print("default callback - topic: " +msg.topic +"  msg: "+str(msg.payload, "utf-8"))
-	
+
 def on_message_from_ping(client, userdata, message):
-	sentmsg = int(message.payload.decode())
-	newmsg = sentmsg+1
-	print("custom callback - pong: "+newmsg)
+	
+	number = int(message.payload)
+	time.sleep(1)
+	print("custom callback - pong: "+str(number))
+	client.publish(message.topic, number+1)
 	
 if __name__ == '__main__':
 	client=mqtt.Client()
+	client.on_message = on_message
 	client.on_connect=on_connect
 	
-	client.connect("pi@172.20.10.10", 1001, 60)
+	client.connect("172.20.10.10", 1883, 60)
 	
 	client.loop_forever()
--- vm_start_chain.py
+++ vm_start_chain.py
@@ -6,18 +6,18 @@ def on_connect(client, userdata, flags, rc):
 	print("connected to server with result code"+str(rc))
 
 if __name__ == '__main__':
-	ip_address = 0
 	ip_address = socket.gethostbyname(socket.gethostname())
 	client = mqtt.Client("asielgar")
 	
 	client.on_connect = on_connect
 	
-	client.connect("pi", 1001, 60)
+	client.connect("172.20.10.10", 1883, 60)
 	
 	client.loop_start()
 	time.sleep(1)
 	
 	while True:
-		client.publish("asielgar/ping", 11, f"{ip_address}")
+		number = int(input("Enter start number: "))
+		client.publish("asielgar/ping", number)
 		print("publishing ping")
 		time.sleep(4)
